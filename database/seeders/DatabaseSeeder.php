0f22696226fcac263aa7ebb3c8945886cfac8199
abhipsx2-hub <abhi.psx2@gmail.com>
2026-04-08 00:40:43 +0530

    Fix faker dependency in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Admin
-        User::factory()->create([
+        User::create([
             'name' => 'Admin User',
             'email' => 'admin@electraize.com',
             'password' => bcrypt('password'),
